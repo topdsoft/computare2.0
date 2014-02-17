b136f378040fe8479600cd4ad8380f2a9d2ee39d
Kurt Lakin <kurtlakin@topdsoft.com>
2014-02-17 16:50:38 -0600

    bugfix

--- computare.sql
+++ computare.sql
@@ -1148,7 +1148,7 @@ CREATE TABLE IF NOT EXISTS `sales` (
   `created_id` int(10) unsigned NOT NULL,
   `item_id` int(10) unsigned NOT NULL,
   `salesOrder_id` int(10) unsigned zerofill NOT NULL,
-  `customer_id` int(10) unsigned NOT NULL,
+  `customer_id` int(10) unsigned zerofill NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
