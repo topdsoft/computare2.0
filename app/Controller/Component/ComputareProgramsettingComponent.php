07d14a37610b5e9bc85e14a9b7d2669c94e7f8d1
Kurt Lakin <kurtlakin@topdsoft.com>
2019-03-19 13:24:47 -0700

    added new tables for vehicle tracking files_vehicles vehicleNotes vehicles vehicleVisits

--- app/Controller/Component/ComputareProgramsettingComponent.php
+++ app/Controller/Component/ComputareProgramsettingComponent.php
@@ -38,7 +38,7 @@ class ComputareProgramsettingComponent extends Component{
 	 */
 	public function updatedb($uid) {
 		//latest schema
-		$latest=6;
+		$latest=7;
 		//get models
 		$this->Programsetting=ClassRegistry::init('Programsetting');
 		$settings=$this->Programsetting->find('first', array('order'=>array('Programsetting.created'=>'desc')));
@@ -219,6 +219,60 @@ CREATE TABLE `files` (
 			if($ok) $settings['Programsetting']['dbschema']=6;
 //NEED LOGGING HERE
 		}//endif 6
+		
+		#dbschema==7
+		if($settings['Programsetting']['dbschema']<7) {
+			//add files table
+			$this->Programsetting->query("
+
+CREATE TABLE `files_vehicles` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `file_id` int(10) UNSIGNED NOT NULL,
+  `vehicle_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `images_vehicles` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `image_id` int(10) UNSIGNED NOT NULL,
+  `vehicle_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `vehicleNotes` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `vehicle_id` int(10) UNSIGNED NOT NULL,
+  `created` datetime NOT NULL,
+  `created_id` int(10) UNSIGNED NOT NULL,
+  `note` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `vehicles` (
+  `id` int(10) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `created_id` int(10) UNSIGNED NOT NULL,
+  `customer_id` int(10) UNSIGNED NOT NULL,
+  `vin` varchar(32) NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `vehicleVisits` (
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `created` datetime NOT NULL,
+  `created_id` int(10) UNSIGNED NOT NULL,
+  `exits` datetime DEFAULT NULL,
+  `exit_id` int(10) UNSIGNED DEFAULT NULL,
+  `vehicle_id` int(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+			");
+//NEED ERROR CATCH HERE
+			if($ok) $settings['Programsetting']['dbschema']=7;
+//NEED LOGGING HERE
+		}//endif 7
 		
 		$settings['Programsetting']['created_id']=$uid;
 		if($ok) $ok=$this->save($settings);
